86e2a3d4f521744a4aff992ecfb9c9f33870a3ad
Shubh <shubhjhawar78@gmail.com>
2023-07-27 00:48:48 -0400

    final commit

--- components/CustomTexts.jsx
+++ components/CustomTexts.jsx
@@ -1,9 +1,9 @@
 'use client';
 
-import {motion} from "framer-motion";
+import { motion } from "framer-motion";
 import { textContainer, textVariant2 } from "../utils/motion";
 
-export const TypingText = ({title, textStyles}) => (
+export const TypingText = ( { title, textStyles } ) => (
   <motion.p
     variants={textContainer}
     className={`font-normal text-[14px] text-secondary-white ${textStyles}`}
